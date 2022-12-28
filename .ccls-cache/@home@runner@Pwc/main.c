f8dad1b8cf46c7d49d88dfadd6a8ef33aa6a9641
Sergio Gabriel <97525050+Sergio-Aquino@users.noreply.github.com>
2022-12-28 16:30:25 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Pwc/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+  char option;
+  
+  printf("Inform your idiom preference:\n");
+  printf("A - Portuguese\n");
+  printf("B - English\n");
+  printf("C - French\n");
+  printf("D - Spanish\n");
+  scanf("%s", &option);
+
+  printf
+  
+  return 0;
+}
